256f3e4f8687b851b5585a9b8dd26a7e69423903
pablo-perez91 <pablo.perez91@educa.madrid.org>
2025-12-05 13:48:15 +0100

    .

---
+++ 4/javaprueba.js
@@ -0,0 +1,13 @@
+        //alert(`prueba de codigo`);
+        var mensaje = `Hola mundo que tal`
+        console.log(`Hola mundo hola que tal`);
+        console.log(mensaje);
+        console.log(document);
+        console.log(localStorage);
+
+        var viernes = true
+        console.log(`huida`+ viernes)
+        //debugger
+        if(viernes == true){
+        console.log(`si es una huida logica`);
+        }
